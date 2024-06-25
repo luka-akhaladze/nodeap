fae7d3fb133a3d5ded96843fa32132f0939ba561
luka akhaladze <luka.akhaladze@makingscience.com>
2024-06-25 15:38:54 +0400

    added node app

---
+++ my-web-server/server.js
@@ -0,0 +1,18 @@
+const http = require('http');
+
+// Define the hostname and port
+const hostname = '0.0.0.0';
+const port = 3000;
+
+// Create the server
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!\n');
+});
+
+// Start the server
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
